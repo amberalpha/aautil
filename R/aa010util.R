727ac0833da062a9a9e0d2f50038249f8a910e1a
unknown <Bloomberg@SILVER-8.silverstreet.lan>
2015-02-17 04:13:54 +0000

    add abbrev

--- R/aa010util.R
+++ R/aa010util.R
@@ -20,6 +20,7 @@ rdroot <- function(){root.global}
 #rdroot <- function(){".."}
 
 #abbrev - abbreviate and remove forbidden characters
+#' @export
 abbrev <- function(x,len=30,rep="",patt=list("\\.","/","&","\\*",":"),nospace=TRUE) {
   if(nospace) patt <- union(patt," ")
   x <- abbreviate(x,minl=len)
